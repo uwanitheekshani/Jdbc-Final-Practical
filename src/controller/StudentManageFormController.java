92039bf7b1531b555a29e30e2aae4ad68299faca
uwanitheekshani <uwanitheekshani@gmail.com>
2022-07-12 11:11:10 +0530

    Create DBConnection

--- src/controller/StudentManageFormController.java
+++ src/controller/StudentManageFormController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentManageFormController {
+    public AnchorPane studentContext;
+    public TableView tblStudent;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtStudentEmail;
+    public JFXTextField txtStudentContact;
+    public JFXTextField txtStudentAddress;
+    public JFXTextField txtStudentNic;
+    public Button btnSaveStudent;
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void studentIdKeyPressed(KeyEvent keyEvent) {
+    }
+
+    public void textField_key_Released(KeyEvent keyEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveStudentOnAction(ActionEvent actionEvent) {
+    }
 }
